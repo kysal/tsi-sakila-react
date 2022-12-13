ed3788124714bc9ef52b0790e2a8d5c0dadccdb2
Kyle <kylesalisbury2@gmail.com>
2022-12-13 09:27:58 +0000

    Added main header function

---
+++ src/components/MainHeader.js
@@ -0,0 +1,11 @@
+import './MainHeader.css';
+
+function MainHeader() {
+    return (
+        <div className="main-title">
+            <h1>Movie Rater</h1>
+        </div>
+    );
+}
+
+export default MainHeader;
