9237e5fbe8fa50095e4d9a60449c9ca59faab8b9
Kyle <kylesalisbury2@gmail.com>
2022-12-14 10:03:18 +0000

    Fixed incorrect page bug in filmlist pagination

--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -1,76 +1,58 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import './FilmList.css';
-
-// class FilmList extends React.Component {
-//     constructor(props) {
-//         super(props)
-//         this.state = {
-//             films: []
-//         }
-//     }
-
-
-//     async getFilms() {
-//         const searchTerm = await new URLSearchParams(window.location.search).get("search");
-//         let films;
-//         if (searchTerm === null) {
-//             let response = await fetch("http://localhost:8080/film")
-//             films = await response.json();
-//             this.setState({films: films})
-//         } else {
-//             let response = await fetch("http://localhost:8080/film/search/" + searchTerm)
-//             films = await response.json();
-//             this.setState({films: films})
-//         }
-
-//     }
-
-//     componentDidMount() {
-//         this.getFilms();
-//     }
-
-
-//     render() {
-//         return (
-//             <div>
-//             <div className="search">
-//                 <form>
-//                 <input id="search" name="search" type="text"/>
-//                 <button className="button-primary" type="submit">Search</button>
-//                 </form>
-//             </div>
-//             <div id="film-list">
-//             {this.state.films.map(film => (<Link to={"/film/" + film.filmId} key={film.filmId}><div className="film-list-item">{film.title}</div></Link>))}
-//             </div>
-//             </div>
-//         );
-//     }
-
-// }
 
 function FilmList() {
 
+    const [page, setPage] = useState(0);
+    const [searchParams, setSearchParams] = useSearchParams();
     const [filmList, setFilmList] = useState([]);
     useEffect(() => {
-        fetchFilmList()
+        fetchFilmList(0)
     }, [])
 
-    const [page, setPage] = useState(1);
+    
+    
+    const [pageInit, setPageInit] = useState(false);
 
-    const fetchFilmList = () => {
-        axios.get('http://localhost:8080/film/page/' + page)
-        .then((res) => {
-            setFilmList(res.data.content);
-        })
+
+    const fetchFilmList = (pageNum) => {
+
+        if (pageNum < 0) return;
+
+        if (!pageInit) {
+            let tempPage = Number(searchParams.get('page')-1);
+            if (tempPage === null) {
+                pageNum = 0;
+            }  else {
+                pageNum = tempPage;
+            }
+            setPageInit(true);
+        }
+
+        if (searchParams.get('search') === null || searchParams.get('search') === "") {
+            axios.get('http://localhost:8080/film/page/' + pageNum)
+            .then((res) => {
+                setFilmList(res.data.content);
+            })
+        } else {
+            axios.get('http://localhost:8080/film/page/' + pageNum + '/search/' + searchParams.get('search'))
+            .then((res) => {
+                setFilmList(res.data.content);
+            })
+        }
+
+        setPage(pageNum);
+        setSearchParams({page: pageNum + 1})
     }
 
-    const changePage = (effect) => {
-        if ((page + effect) > 1) {
-            setPage(page + effect)
-            fetchFilmList();
-        }
+    const search = (searchTerm) => {
+        setSearchParams({
+            page: 0,
+            search: searchTerm
+        });
+        fetchFilmList();
     }
 
     return (
@@ -78,11 +60,11 @@ function FilmList() {
             <div className="search">
                 <form>
                     <input id="search" name="search" type="text"/>
-                    <button className="button-primary" type="submit">Search</button>
+                    <button className="button-primary" type="button" onClick={() => search("test")}>Search</button>
                 </form>
-                <button className="button-primary" onClick={() => changePage(-1)}>{"<-"}</button>
-                <span> {page} </span>
-                <button className="button-primary" onClick={() => changePage(1)}>{"->"}</button>
+                <button className="button-primary" onClick={() => fetchFilmList(page-1)}>{"<-"}</button>
+                <span> {page+1} </span>
+                <button className="button-primary" onClick={() => fetchFilmList(page+1)}>{"->"}</button>
             </div>
             <div id="film-list">
                 {filmList.map(film => (<Link to={"/film/" + film.filmId} key={film.filmId}><div className="film-list-item">{film.title}</div></Link>))}
